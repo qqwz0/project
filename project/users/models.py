265959e318777e9149d4997af5786fa091e389a3
Nastya <yanyshynnastya3@gmail.com>
2025-01-10 19:10:30 +0200

    Created a User model

--- project/users/models.py
+++ project/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('Студент', 'Студент'),
+        ('Викладач', 'Викладач'),
+    ]
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Студент')
+    academic_group = models.CharField(max_length=6, blank=True, null=True)  # For students
+    department = models.CharField(max_length=100, blank=True, null=True)   # For teachers
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['role']  # Specify any additional fields required when creating a superuser
+
+    def __str__(self):
+        return self.email
+
