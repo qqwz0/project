e1db1299c116f8e4adf2bafc0805408e973c28e9
vikmrnk <kmrnkviktoria@gmail.com>
2025-06-21 20:34:25 +0300

    Enhance user profile picture handling in context processor to check for valid Cloudinary URLs before returning the profile picture. Default avatar is returned if no valid picture is found.

--- project/apps/users/context_processors.py
+++ project/apps/users/context_processors.py
@@ -3,8 +3,11 @@ from .models import CustomUser
 
 def user_profile_picture(request):
     if request.user.is_authenticated:
-        if request.user.profile_picture:
-            return {'profile_picture_url': request.user.profile_picture.url}
-        else:
-            return {'profile_picture_url': static('images/default-avatar.jpg')}
+        if request.user.profile_picture and hasattr(request.user.profile_picture, 'url'):
+            picture_url = request.user.profile_picture.url
+            if 'res.cloudinary.com' in picture_url:
+                return {'profile_picture_url': picture_url}
+
+        return {'profile_picture_url': static('images/default-avatar.jpg')}
+
     return {}
