adeaeadde1b4b4e6b81ced83e74f4abe85594e6a
Nastya <yanyshynnastya3@gmail.com>
2025-09-09 22:41:58 +0300

    Add announcements model and admin, enhance verbose names

--- project/apps/catalog/admin.py
+++ project/apps/catalog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import OnlyTeacher
+from .models import OnlyTeacher, Announcement
 
 admin.site.register(OnlyTeacher)
 
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ("title", "author_type", "author_faculty", "author_department", "announcement_type", "is_active", "created_at")
+    list_filter = ("author_type", "announcement_type", "is_active", "author_department","author_faculty")
+    search_fields = ("title", "content")
